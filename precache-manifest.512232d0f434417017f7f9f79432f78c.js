2356baf0bcbb7054033a57eb780cdb99b05d3b81
Artemchuk Anastasia <artemchuk.nastya@gmail.com>
2018-12-18 21:00:28 +0300

    Updates

---
+++ precache-manifest.512232d0f434417017f7f9f79432f78c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3a455371bf08e9596a6",
+    "url": "/simple_synthesizer/static/js/runtime~main.f3a45537.js"
+  },
+  {
+    "revision": "3f0bfac8cba9178a40ed",
+    "url": "/simple_synthesizer/static/js/main.3f0bfac8.chunk.js"
+  },
+  {
+    "revision": "49374628d1a39d3f6563",
+    "url": "/simple_synthesizer/static/js/1.49374628.chunk.js"
+  },
+  {
+    "revision": "3f0bfac8cba9178a40ed",
+    "url": "/simple_synthesizer/static/css/main.06de4f25.chunk.css"
+  },
+  {
+    "revision": "49374628d1a39d3f6563",
+    "url": "/simple_synthesizer/static/css/1.d76e170d.chunk.css"
+  },
+  {
+    "revision": "77fb64316a6b77d99ac60168034c222e",
+    "url": "/simple_synthesizer/index.html"
+  }
+];
